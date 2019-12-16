d1d730e6ad586c79cfd72fa615a49eb56fdc4ee1
perjerz3434 <perjerz3434@gmail.com>
2019-12-17 00:25:03 +0700

    4 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-componet',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
